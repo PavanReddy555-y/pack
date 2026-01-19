1605234ed4d1bf67c4204ec503003144e72b357d
PavanReddyCn <pavankumar.yerraguntla@therealtyadvisors.in>
2026-01-19 20:34:16 +0530

    initial

---
+++ expe/src/main/java/com/pone/expe/ExpeApplication.java
@@ -0,0 +1,13 @@
+package com.pone.expe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpeApplication.class, args);
+	}
+
+}
---
+++ expe/src/test/java/com/pone/expe/ExpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pone.expe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
